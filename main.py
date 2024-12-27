88091e28bf68a90a0f52db8a55abdd59cf3d9ba5
Abhay <abhaykoli214@gmail.com>
2024-12-27 23:04:08 +0530

    changes

--- main.py
+++ main.py
@@ -56,4 +56,4 @@ app.include_router(orderlist.router, tags=['order route'])
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
